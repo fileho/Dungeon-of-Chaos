af15eb280f2106081933a96cd5635ea94f536d4c
Martina Fusková <sachistka.marta@gmail.com>
2022-09-30 16:04:15 +0200

    Tab Button Renaming

--- Dungeon of Chaos/Assets/Scripts/UI/CharacterSheet/TabMenu/TabBtn.cs
+++ Dungeon of Chaos/Assets/Scripts/UI/CharacterSheet/TabMenu/TabBtn.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class TabBtn : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public TabGroup tabGroup;
     [HideInInspector]
