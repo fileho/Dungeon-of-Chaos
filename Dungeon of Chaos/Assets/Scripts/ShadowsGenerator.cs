6488b677a8ff82d88f05a2e00d7762e776f0ec52
fileho <jiri.filek@gmail.com>
2022-11-03 21:51:08 +0100

    Multiple shadow casters

--- Dungeon of Chaos/Assets/Scripts/ShadowsGenerator.cs
+++ Dungeon of Chaos/Assets/Scripts/ShadowsGenerator.cs
@@ -1,36 +1,69 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 using Vector3 = UnityEngine.Vector3;
 
+
+// Generated all the shadow casters 2D for walls
 [ExecuteInEditMode]
 public class ShadowsGenerator : MonoBehaviour
 {
-    private ShadowCaster2D shadowCaster2D;
     [SerializeField]
     private Tilemap tilemap;
 
+    private HashSet<Vector3Int> visited;
+
     public void BakeShadows()
     {
-        shadowCaster2D = GetComponent<ShadowCaster2D>();
+        Setup();
         BindingFlags accessFlagsPrivate = BindingFlags.NonPublic | BindingFlags.Instance;
-        FieldInfo shapePathField = typeof(ShadowCaster2D).GetField("m_ShapePath", accessFlagsPrivate);
-        shapePathField.SetValue(shadowCaster2D, TrackEdges());
+        while (true)
+        {
+            var f = FindFilledArea();
+            // All shadows are constructed
+            if (!f.HasValue)
+                break;
+
+            var o = new GameObject("ShadowCaster");
+            o.transform.parent = transform;
+            ShadowCaster2D shadowCaster2D = o.AddComponent<ShadowCaster2D>();
+
+            // Use reflection since m_ShapePath is private without any wrappers
+            // It is still in experimental phase
+            FieldInfo shapePathField = typeof(ShadowCaster2D).GetField("m_ShapePath", accessFlagsPrivate);
+            shapePathField.SetValue(shadowCaster2D, TrackEdges(f.Value));
+        }
+
+        // scene has to be reload for the shadows to rebuild
+#if UNITY_EDITOR
+        EditorSceneManager.OpenScene(SceneManager.GetActiveScene().path); 
+#endif
     }
 
-    Vector3[] TrackEdges()
+    private void Setup()
     {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+        tilemap.CompressBounds();
+        visited = new HashSet<Vector3Int>();
+    }
+
+    // Finds all corners for the shadow generation
+    Vector3[] TrackEdges(Vector3Int start)
+    {
+        // use two lists, one for inner point, the other for outer point
         List<Vector3> inner = new List<Vector3>();
         List<Vector3> outer = new List<Vector3>();
-
-        Vector3Int start = FindFilledArea();
 
         Vector3Int dir = Vector3Int.right;
         Vector3Int current = TrackEdge(start, ref dir, inner, outer);
@@ -52,6 +85,7 @@ public class ShadowsGenerator : MonoBehaviour
         while (tilemap.HasTile(current + dir))
         {
             current += dir;
+            visited.Add(current);
         }
 
         var nextDir = GetNextDir(current, dir);
@@ -61,15 +95,15 @@ public class ShadowsGenerator : MonoBehaviour
         return current;
     }
 
-    private Vector3Int FindFilledArea()
+    private Vector3Int? FindFilledArea()
     {
         foreach (var pos in tilemap.cellBounds.allPositionsWithin)
         {
-            if (tilemap.HasTile(pos))
+            if (tilemap.HasTile(pos) && !visited.Contains(pos))
                 return pos;
         }
 
-        throw new Exception("No tile found");
+        return null;
     }
 
     Vector3Int GetNextDir(Vector3Int pos, Vector3Int dir)
@@ -83,8 +117,8 @@ public class ShadowsGenerator : MonoBehaviour
         const int scale = 4;
 
         var mid = (pos + new Vector3(0.5f, 0.5f)) * scale;
-        var p1 = mid + (Vector3)(newDir - oldDir) * 0.5f * scale;
-        var p2 = mid + (Vector3)(oldDir - newDir) * 0.5f * scale;
+        var p1 = mid + 0.5f * scale * (Vector3)(newDir - oldDir);
+        var p2 = mid + 0.5f * scale * (Vector3)(oldDir - newDir);
 
         if (inner.Count == 0)
         {
@@ -95,7 +129,7 @@ public class ShadowsGenerator : MonoBehaviour
 
         var tmp = inner[inner.Count - 1];
         const float limit = 0.1f;
-
+        // find next point to add to each list
         if (oldDir.y != 0)
         {
             if (Math.Abs(tmp.x - p1.x) < limit)
@@ -142,12 +176,14 @@ public class ShadowsGeneratorEditor : Editor
     {
         serializedObject.Update();
 
+        EditorGUILayout.PropertyField(tilemap);
+
         if (GUILayout.Button("Bake Shadows"))
         {
             generator.BakeShadows();
+            // need to return since the scene will be reopened from code to reload shadows
+            return;
         }
-
-        EditorGUILayout.PropertyField(tilemap);
 
         serializedObject.ApplyModifiedProperties();
     }
