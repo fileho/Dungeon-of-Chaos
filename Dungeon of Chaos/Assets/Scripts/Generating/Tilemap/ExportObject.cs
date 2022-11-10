54aa1113fbfebdfae949af6a3235462860fd84f2
fileho <jiri.filek@gmail.com>
2022-11-10 18:23:56 +0100

    Lights and rocks

---
+++ Dungeon of Chaos/Assets/Scripts/Generating/Tilemap/ExportObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExportObject : ExportTile
+{
+    [SerializeField]
+    private GameObject objectToSpawn;
+
+    public override void Place(Tilemaps maps, Vector3Int pos)
+    {
+        float scale = maps.objects.parent.localScale.x;
+
+        var o =
+            Instantiate(objectToSpawn, (pos + new Vector3(0.5f, 0.5f, 0)) * scale, Quaternion.identity, maps.objects);
+    }
+}
