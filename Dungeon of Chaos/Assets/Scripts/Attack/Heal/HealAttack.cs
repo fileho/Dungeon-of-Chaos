4a7aefdc9d2acafa97de662e672ebea9c2300883
Shivam Sharma <sharma.shivam09@gmail.com>
2022-11-29 00:13:37 +0100

    - Heal and Summon Indicator added

--- Dungeon of Chaos/Assets/Scripts/Attack/Heal/HealAttack.cs
+++ Dungeon of Chaos/Assets/Scripts/Attack/Heal/HealAttack.cs
@@ -20,6 +20,14 @@ public class HealAttack : IAttack
 
     protected override IEnumerator StartAttackAnimation()
     {
+        Vector3 weaponPos = Weapon.transform.position;
+        Vector3 targetDirection = (GetTargetPosition() - (Vector2)weaponPos).normalized;
+
+        Vector3 startPos = Weapon.transform.localPosition;
+        Vector3 endPos = startPos + (targetDirection);
+        endPos = Weapon.transform.lossyScale.x > 0 ? endPos : -Vector3.Reflect(endPos, Vector2.up);
+        Vector3 midPos = (endPos + startPos) / 2f;
+
         IIndicator indicator = CreateIndicator(transform);
         if (indicator)
         {
@@ -28,11 +36,43 @@ public class HealAttack : IAttack
             yield return new WaitForSeconds(indicator.Duration);
         }
 
-        PrepareWeapon();
-        Heal();
 
-        //SoundManager.instance.PlaySound(swingSFX);
-        ResetWeapon();
+        // Cache weapon rotation to restore after the animation
+        var initialAssetRotation = Weapon.Asset.localRotation;
+        Weapon.Asset.localRotation = Quaternion.Euler(0, 0, Weapon.GetUprightAngle());
+
+
+        float attackAnimationDurationOneWay = AttackAnimationDuration / 2f;
+
+        // Forward
+        float time = 0;
+        midPos -= new Vector3(0, 0.1f);
+        while (time <= 1)
+        {
+            time += (Time.deltaTime / attackAnimationDurationOneWay);
+            float currentPos = Tweens.EaseInCubic(time);
+            Weapon.transform.localPosition = Vector3.Slerp(startPos - midPos, endPos - midPos, currentPos) + midPos;
+            yield return null;
+        }
+
+        // Backward
+        time = 0;
+        midPos += new Vector3(0, 0.2f);
+        while (time <= 1)
+        {
+            time += (Time.deltaTime / attackAnimationDurationOneWay);
+            float currentPos = Tweens.EaseOutCubic(time);
+            Weapon.transform.localPosition = Vector3.Slerp(endPos - midPos, startPos - midPos, currentPos) + midPos;
+            yield return null;
+        }
+
+        SoundManager.instance.PlaySound(swingSFX);
+
+        // Reset
+        Weapon.Asset.localRotation = initialAssetRotation;
+        Weapon.transform.localPosition = startPos;
+
+        Heal();
         isAttacking = false;
     }
 
