21c98c250806c6ad68d14615ac48321459a1031f
Martina Fusková <sachistka.marta@gmail.com>
2022-12-01 14:50:45 +0100

    Loot
    - Boss drops teleport
    - Chests essence value customization

--- Dungeon of Chaos/Assets/Scripts/Loot/Essence.cs
+++ Dungeon of Chaos/Assets/Scripts/Loot/Essence.cs
@@ -54,6 +54,11 @@ public class Essence : MonoBehaviour
         }
     }
 
+    public void SetValue(float val)
+    {
+        value = val;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!collision.collider.CompareTag("Player")) return;
