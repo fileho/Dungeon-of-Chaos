a2c28630468367a45f7c72e11021c06e3b6c2c93
Martina Fusková <sachistka.marta@gmail.com>
2022-12-09 22:12:12 +0100

    Character Footsteps SFX

--- Dungeon of Chaos/Assets/Scripts/Movement/CharacterMovement.cs
+++ Dungeon of Chaos/Assets/Scripts/Movement/CharacterMovement.cs
@@ -9,6 +9,9 @@ public class CharacterMovement : IMovement
     private Rigidbody2D rb;
     private Animator animator;
 
+    [SerializeField] private SoundSettings footstepsSFX;
+    private SoundData sfx;
+
     public override IMovement Init(Transform transform, Stats stats)
     {
         this.stats = stats;
@@ -34,11 +37,14 @@ public class CharacterMovement : IMovement
             moveDir = dir;
             animator.SetBool("isMoving", true);
             // TODO ADD this with animations
-            // SoundManager.instance.PlaySound(footstepsSFX);
+            if (sfx == null)
+                sfx = SoundManager.instance.PlaySoundLooping(footstepsSFX);
         }
         else
         {
             animator.SetBool("isMoving", false);
+            SoundManager.instance.StopLoopingSound(sfx);
+            sfx = null;
         }
 
         rb.AddForce(stats.MovementSpeed() * Time.fixedDeltaTime * 1000 * dir);
