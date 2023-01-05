4817b3da0f37ae475e488ec2d8f05962c8836396
Martina Fusková <sachistka.marta@gmail.com>
2023-01-05 19:21:57 +0100

    Crystal

---
+++ Dungeon of Chaos/Assets/Scripts/Loot/Crystal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    [SerializeField] private float hpBoost;
+    [SerializeField] private float manaBoost;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Character.instance.stats.RegenerateHealth(hpBoost);
+            Character.instance.stats.RegenerateMana(manaBoost);
+            Destroy(gameObject);
+        }
+    }
+}
