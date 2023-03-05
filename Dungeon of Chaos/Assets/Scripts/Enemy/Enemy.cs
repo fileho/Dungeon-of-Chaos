ab0aa543a298f06ab272a6a21d891bc9e14f6bac
Shivam Sharma <sharma.shivam09@gmail.com>
2023-03-05 12:38:43 +0100

    - Enemy attacking after kill fix

--- Dungeon of Chaos/Assets/Scripts/Enemy/Enemy.cs
+++ Dungeon of Chaos/Assets/Scripts/Enemy/Enemy.cs
@@ -2,205 +2,197 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 
-[RequireComponent(typeof(AttackManager))]
-public class Enemy : Unit
-{
-    private const float CHASE_HEAT = 3f;
-    private const float RAYCAST_TIME_INTERVAL = 1f;
+[RequireComponent (typeof (AttackManager))]
+public class Enemy : Unit {
+	private const float CHASE_HEAT = 3f;
+	private const float RAYCAST_TIME_INTERVAL = 1f;
 
-    private enum State
-    {
-        Idle,
-        Chase,
-        Attack,
-    }
+	private enum State {
+		Idle,
+		Chase,
+		Attack,
+	}
 
-    [SerializeField]
-    private ILoot loot;
-    public LootModifiers lootModifiers;
-    private AttackManager attackManager;
-    private IAttack currentAttack;
-    private State state;
-    private Animator animator;
-    private Rigidbody2D rb;
+	[SerializeField]
+	private ILoot loot;
+	public LootModifiers lootModifiers;
+	private AttackManager attackManager;
+	private IAttack currentAttack;
+	private State state;
+	private Animator animator;
+	private Rigidbody2D rb;
 
-    [Header("Enemy Ambient")]
-    [SerializeField]
-    private float maxDistance;
-    [SerializeField]
-    private SoundSettings ambientSFX;
-    private const float minFrequency = 2f;
-    private const float maxFrequency = 5.5f;
-    private float frequency = 0f;
-    private float time = 0f;
+	[Header ("Enemy Ambient")]
+	[SerializeField]
+	private float maxDistance;
+	[SerializeField]
+	private SoundSettings ambientSFX;
+	private const float minFrequency = 2f;
+	private const float maxFrequency = 5.5f;
+	private float frequency = 0f;
+	private float time = 0f;
 
-    protected override void Init()
-    {
-        loot = Instantiate(loot).Init(this);
-        lootModifiers = Instantiate(lootModifiers);
-        Target = Character.instance;
-        state = State.Idle;
-        attackManager = GetComponent<AttackManager>();
-        animator = GetComponent<Animator>();
-        rb = transform.GetComponent<Rigidbody2D>();
-    }
+	protected override void Init ()
+	{
+		loot = Instantiate (loot).Init (this);
+		lootModifiers = Instantiate (lootModifiers);
+		Target = Character.instance;
+		state = State.Idle;
+		attackManager = GetComponent<AttackManager> ();
+		animator = GetComponent<Animator> ();
+		rb = transform.GetComponent<Rigidbody2D> ();
+	}
 
-    private float lastLosTime = Mathf.NegativeInfinity;
-    private float lastRayCastCheck = 0f;
+	private float lastLosTime = Mathf.NegativeInfinity;
+	private float lastRayCastCheck = 0f;
 
-    private bool IsTargetInChaseRange()
-    {
-        Vector2 direction = GetTargetDirection();
+	private bool IsTargetInChaseRange ()
+	{
+		Vector2 direction = GetTargetDirection ();
 
-        bool playerHit = false;
+		bool playerHit = false;
 
-        if (Time.time - lastRayCastCheck > RAYCAST_TIME_INTERVAL)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                Vector2 dir = Quaternion.AngleAxis((-5 + (1 * i)), Vector3.forward) * direction;
-                RaycastHit2D hit =
-                    Physics2D.Linecast(transform.position, (Vector2)transform.position + (dir * stats.ChaseDistance()),
-                                       ~(1 << LayerMask.NameToLayer("Enemy")));
+		if (Time.time - lastRayCastCheck > RAYCAST_TIME_INTERVAL) {
+			for (int i = 0; i < 10; i++) {
+				Vector2 dir = Quaternion.AngleAxis ((-5 + (1 * i)), Vector3.forward) * direction;
+				RaycastHit2D hit =
+					Physics2D.Linecast (transform.position, (Vector2)transform.position + (dir * stats.ChaseDistance ()),
+									   ~(1 << LayerMask.NameToLayer ("Enemy")));
 
-                if (hit.collider && hit.collider.CompareTag("Player"))
-                {
-                    playerHit = true;
-                    lastLosTime = Time.time;
-                    break;
-                }
-            }
-            lastRayCastCheck = Time.time;
-        }
-        return playerHit || (Time.time - lastLosTime < CHASE_HEAT);
-    }
+				if (hit.collider && hit.collider.CompareTag ("Player")) {
+					playerHit = true;
+					lastLosTime = Time.time;
+					break;
+				}
+			}
+			lastRayCastCheck = Time.time;
+		}
+		return playerHit || (Time.time - lastLosTime < CHASE_HEAT);
+	}
 
-    private void FixedUpdate()
-    {
-        animator.SetBool("isMoving", rb.velocity.magnitude > 0.01f);
-        SwitchEnemyStates();
-        if (ShouldPlaySound())
-            PlayAmbientSound();
-    }
+	private void FixedUpdate ()
+	{
+		animator.SetBool ("isMoving", rb.velocity.magnitude > 0.01f);
+		SwitchEnemyStates ();
+		if (ShouldPlaySound ())
+			PlayAmbientSound ();
+	}
 
-    private bool IsAttacking()
-    {
-        return currentAttack != null && currentAttack.IsAttacking();
-    }
+	private bool IsAttacking ()
+	{
+		return currentAttack != null && currentAttack.IsAttacking ();
+	}
 
-    private void RotateWeapon()
-    {
-        if (weapon != null)
-        {
-            weapon.RotateWeapon(GetTargetPosition());
-        }
-    }
+	private void RotateWeapon ()
+	{
+		if (weapon != null) {
+			weapon.RotateWeapon (GetTargetPosition ());
+		}
+	}
 
-    private void ResetWeapon()
-    {
-        if (weapon != null)
-        {
-            weapon.ResetWeapon();
-        }
-    }
+	private void ResetWeapon ()
+	{
+		if (weapon != null) {
+			weapon.ResetWeapon ();
+		}
+	}
 
-    private void FlipSprite()
-    {
-        Vector2 dir = GetTargetDirection();
+	private void FlipSprite ()
+	{
+		Vector2 dir = GetTargetDirection ();
 
-        if (dir.x > 0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
-        else if (dir.x < -0.01f)
-            transform.localScale = Vector3.one;
-    }
+		if (dir.x > 0.01f)
+			transform.localScale = new Vector3 (-1, 1, 1);
+		else if (dir.x < -0.01f)
+			transform.localScale = Vector3.one;
+	}
 
-    private bool Attack()
-    {
-        if (!IsTargetInChaseRange() || IsAttacking())
-            return true;
-        currentAttack = attackManager.GetBestAvailableAttack();
-        FlipSprite();
-        RotateWeapon();
-        if (currentAttack != null)
-        {
-            state = State.Attack;
-            currentAttack.Attack();
-            return true;
-        }
-        return false;
-    }
+	private bool Attack ()
+	{
+		if (!IsTargetInChaseRange () || IsAttacking ())
+			return true;
+		currentAttack = attackManager.GetBestAvailableAttack ();
+		FlipSprite ();
+		RotateWeapon ();
+		if (currentAttack != null) {
+			state = State.Attack;
+			currentAttack.Attack ();
+			return true;
+		}
+		return false;
+	}
 
-    private void Move()
-    {
-        movement.Move();
-    }
+	private void Move ()
+	{
+		movement.Move ();
+	}
 
-    private bool Chase()
-    {
-        if (IsTargetInChaseRange())
-        {
-            state = State.Chase;
+	private bool Chase ()
+	{
+		if (IsTargetInChaseRange ()) {
+			state = State.Chase;
 
-            if (GetTargetDistance() >= attackManager.GetMinimumAttackRange())
-            {
-                Move();
-            }
-            FlipSprite();
-            RotateWeapon();
-            return true;
-        }
-        return false;
-    }
+			if (GetTargetDistance () >= attackManager.GetMinimumAttackRange ()) {
+				Move ();
+			}
+			FlipSprite ();
+			RotateWeapon ();
+			return true;
+		}
+		return false;
+	}
 
-    private void Idle()
-    {
-        state = State.Idle;
-        ResetWeapon();
-    }
+	private void Idle ()
+	{
+		state = State.Idle;
+		ResetWeapon ();
+	}
 
-    private void SwitchEnemyStates()
-    {
-        if (dead)
-            return;
-        if (Attack())
-        {
-        }
-        else if (Chase())
-        {
-        }
-        else
-        {
-            Idle();
-        }
-    }
+	private void SwitchEnemyStates ()
+	{
+		if (dead)
+			return;
+		if (Attack ()) {
+		} else if (Chase ()) {
+		} else {
+			Idle ();
+		}
+	}
 
-    public bool IsAwake()
-    {
-        return state == State.Attack || state == State.Chase;
-    }
+	public bool IsAwake ()
+	{
+		return state == State.Attack || state == State.Chase;
+	}
 
-    protected override void CleanUp()
-    {
-        loot.Drop();
-        Destroy(transform.parent.gameObject);
-    }
+	protected override void CleanUp ()
+	{
+		loot.Drop ();
+		Destroy (transform.parent.gameObject);
+	}
 
-    private void PlayAmbientSound()
-    {
-        time = 0f;
-        frequency = Random.Range(minFrequency, maxFrequency);
+	private void PlayAmbientSound ()
+	{
+		time = 0f;
+		frequency = Random.Range (minFrequency, maxFrequency);
 
-        float distance = Vector2.Distance(Character.instance.transform.position, transform.position);
-        if (distance > maxDistance)
-            return;
+		float distance = Vector2.Distance (Character.instance.transform.position, transform.position);
+		if (distance > maxDistance)
+			return;
 
-        ambientSFX.SetVolumeFromDistance(distance, maxDistance);
-        SoundManager.instance.PlaySound(ambientSFX);
-    }
+		ambientSFX.SetVolumeFromDistance (distance, maxDistance);
+		SoundManager.instance.PlaySound (ambientSFX);
+	}
 
-    private bool ShouldPlaySound()
-    {
-        time += Time.deltaTime;
-        return time >= frequency;
-    }
+	private bool ShouldPlaySound ()
+	{
+		time += Time.deltaTime;
+		return time >= frequency;
+	}
+
+	protected override void Die ()
+	{
+		if (currentAttack != null)
+			currentAttack.StopAttackIfDead ();
+		base.Die ();
+	}
 }
